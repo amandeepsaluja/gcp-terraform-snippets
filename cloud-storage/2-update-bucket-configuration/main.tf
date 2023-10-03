3a6a925d424461d9e3f9c8cd3b1c66cb1d3f2332
Aman <104459414+amandeep-saluja-whitehorse@users.noreply.github.com>
2023-10-03 17:41:16 -0400

    Update main.tf

--- cloud-storage/2-update-bucket-configuration/main.tf
+++ cloud-storage/2-update-bucket-configuration/main.tf
@@ -1,10 +1,19 @@
 # updating the bucket to enable versioning
 
-resource "google_storage_bucket" "bucket" {
+resource "google_storage_bucket" "versioning" {
   name     = var.bucket_name
   location = var.bucket_location
 
   versioning {
     enabled = true
+  }
+
+  lifecycle_rule {
+    condition {
+      num_newer_versions = 3
+    }
+    action {
+      type = "Delete"
+    }
   }
 }
