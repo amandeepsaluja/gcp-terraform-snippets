ffd9c58b5845e50e4dd970112772871a1e9fc6ba
Aman <104459414+amandeep-saluja-whitehorse@users.noreply.github.com>
2023-10-03 17:27:29 -0400

    Update main.tf

--- cloud-storage/2-update-bucket-configuration/main.tf
+++ cloud-storage/2-update-bucket-configuration/main.tf
@@ -4,5 +4,7 @@ resource "google_storage_bucket" "object_versioning" {
   name     = var.bucket_name
   location = var.bucket_location
 
-  versioning = true
+  versioning {
+    enabled = true
+  }
 }
