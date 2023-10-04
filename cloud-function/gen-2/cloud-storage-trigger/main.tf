e678247b76f1210af51555a62a91b1f06654a346
Aman <104459414+amandeep-saluja-whitehorse@users.noreply.github.com>
2023-10-04 00:28:47 -0400

    Update main.tf

--- cloud-function/gen-2/cloud-storage-trigger/main.tf
+++ cloud-function/gen-2/cloud-storage-trigger/main.tf
@@ -1,24 +1,3 @@
-# generating zip file name for the cloud function
-locals {
-  cf_zip_archive_name = "${var.cloud_function_name}-${data.archive_file.cf_source_zip.output_sha}.zip"
-}
-
-# creating the zip file
-data "archive_file" "cf_source_zip" {
-  type        = "zip"
-  output_path = "/tmp/${var.name}.zip"
-  source_dir  = "${path.module}/../src"
-}
-
-# uploading the zip file to the bucket
-resource "google_storage_bucket_object" "cf_source_zip" {
-  name         = local.cf_zip_archive_name
-  source       = data.archive_file.cf_source_zip.output_path
-  content_type = "application/zip"
-  bucket       = google_storage_bucket.cf_source_archive_bucket.name
-}
-
-
 resource "google_cloudfunctions2_function" "function" {
   name        = var.name
   location    = var.location
@@ -44,4 +23,24 @@ resource "google_cloudfunctions2_function" "function" {
     ingress_settings               = var.ingress_settings
     all_traffic_on_latest_revision = var.all_traffic_on_latest_revision
   }
+}
+
+# generating zip file name for the cloud function
+locals {
+  cf_zip_archive_name = "${var.cloud_function_name}-${data.archive_file.cf_source_zip.output_sha}.zip"
+}
+
+# creating the zip file
+data "archive_file" "cf_source_zip" {
+  type        = "zip"
+  output_path = "/tmp/${var.name}.zip"
+  source_dir  = "${path.module}/../src"
+}
+
+# uploading the zip file to the bucket
+resource "google_storage_bucket_object" "cf_source_zip" {
+  name         = local.cf_zip_archive_name
+  source       = data.archive_file.cf_source_zip.output_path
+  content_type = "application/zip"
+  bucket       = google_storage_bucket.cf_source_archive_bucket.name
 }
