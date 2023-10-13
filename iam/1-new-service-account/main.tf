ae686168371718effd2a070aaddf836b7d2874af
Aman <104459414+amandeep-saluja-whitehorse@users.noreply.github.com>
2023-10-12 22:42:40 -0400

    added content

--- iam/1-new-service-account/main.tf
+++ iam/1-new-service-account/main.tf
@@ -5,4 +5,25 @@ resource "google_service_account" "service_account" {
   description  = var.description
 }
 
+# create workload identity pool
+resource "google_iam_workload_identity_pool" "workload_identity_pool" {
+  workload_identity_pool_id = var.workload_identity_pool_id
+  display_name              = var.workload_identity_pool_name
+  description               = var.workload_identity_pool_description
+}
 
+# create workload identity provider
+resource "google_iam_workload_identity_pool_provider" "provider" {
+  workload_identity_pool_id          = var.workload_identity_pool_id
+  workload_identity_pool_provider_id = var.workload_identity_provider_id
+  display_name                       = var.workload_identity_provider_name
+  description                        = var.workload_identity_provider_description
+
+  attribute_mapping = {
+    "google.subject" = "assertion.repository+assertion.ref"
+  }
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
--- iam/1-new-service-account/variables.tf
+++ iam/1-new-service-account/variables.tf
@@ -13,7 +13,7 @@ variable "region" {
 variable "account_id" {
   type        = string
   description = "Service account ID"
-  default     = "github-cloud-composer-sa"
+  default     = ""
 }
 
 variable "display_name" {
@@ -27,4 +27,44 @@ variable "description" {
   description = "Service account description"
   default     = "Service account for GitHub gcp-airflow-cloud-composer-pipeline repository"
 }
+
+# Pool variables
+variable "workload_identity_pool_id" {
+  type        = string
+  description = "Workload identity pool ID"
+  default     = "github-cloud-composer-pool"
+}
+
+variable "workload_identity_pool_name" {
+  type        = string
+  description = "Workload identity pool Name"
+  default     = "GitHub Pool"
+}
+
+variable "workload_identity_pool_description" {
+  type        = string
+  description = "Workload identity pool Description"
+  default     = "Workload identity pool for GitHub gcp-airflow-cloud-composer-pipeline repository"
+}
+
+# Provider variables
+variable "workload_identity_provider_id" {
+  type        = string
+  description = "Workload identity provider ID"
+  default     = "github-cloud-composer-provider"
+}
+
+variable "workload_identity_provider_name" {
+  type        = string
+  description = "Workload identity provider Name"
+  default     = "GitHub Provider"
+}
+
+variable "workload_identity_provider_description" {
+  type        = string
+  description = "Workload identity provider Description"
+  default     = "Workload identity provider for GitHub gcp-airflow-cloud-composer-pipeline repository"
+}
+
+
 
