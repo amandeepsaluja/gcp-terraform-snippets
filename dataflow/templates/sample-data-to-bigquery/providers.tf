5bcc2233e919df7c91b54154ee22e845104f6ff3
Aman <104459414+amandeep-saluja-whitehorse@users.noreply.github.com>
2023-10-14 14:55:53 -0400

    test docker authentication

---
+++ dataflow/templates/sample-data-to-bigquery/providers.tf
@@ -0,0 +1,16 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "gcp-practice-project-aman-terraform-state-bucket"
+    prefix = "dataflow/templates/sample-data-to-bigquery"
+  }
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
